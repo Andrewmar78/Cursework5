e85b57efa324a11fd6bea84092ce7a8a999d8a36
msi <Andrewmar78>
2022-08-22 21:33:17 +0500

    new 2

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class BaseConfig:
     TESTING = False
     PERMANENT_SESSION_LIFETIME = 600
     # PATH_TO_EQUIPMENT = os.path.join(os.path.dirname(BASEDIR), "coursework_5/data/equipment.json")
-    PATH_TO_EQUIPMENT = os.path.join(os.path.dirname(BASEDIR), "data/equipment.json")
+    PATH_TO_EQUIPMENT = os.path.join(os.path.dirname(BASEDIR), "code/data/equipment.json")
